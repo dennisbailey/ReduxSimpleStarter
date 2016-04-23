52dcd930771b8865bd1f51fdf0c57dff07a30387
Dennis Bailey <bailey.dennis@gmail.com>
2016-04-23 13:47:12 -0600

    creating a videolist component

---
+++ src/components/VideoList.jsx
@@ -0,0 +1,11 @@
+// Here's a functional component. Function components don't worry about state. They just render stuff
+
+import React, from ''react';
+
+const VideoList = (props) => {
+  return (
+    <div>Im a list</div>
+  )
+};
+
+export default VideoList;
