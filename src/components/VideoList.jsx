859fd1daf3dc6e02a3c79a841b792cafd63a421e
Dennis Bailey <bailey.dennis@gmail.com>
2016-04-23 14:00:08 -0600

    Added comments

--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -1,6 +1,7 @@
 // Here's a functional component. Function components don't worry about state. They just render stuff
 // Less expensive than a class component
 // functional components use props instead of this.props
+// The key value lets react identify items in the list. If it knows a key, it'll only update that specific element instead of re-rendering everything
 import React from 'react';
 
 const VideoList = (props) => {
