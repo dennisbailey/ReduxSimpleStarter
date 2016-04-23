566963324ea213638a6120f6a01fa9e16aefa731
Dennis Bailey <bailey.dennis@gmail.com>
2016-04-23 12:25:38 -0600

    example component

---
+++ src/index.js
@@ -0,0 +1,18 @@
+// ES6 syntax to require
+// var React = require('react')
+import React from 'react';
+//Helper method to attach react to the DOM
+import ReactDom from 'react-dom';
+
+class App extends React.Component {
+  // You must have a render funtion within every react component
+  render() {
+    // render components can only return one element. But that element can wrap around multiple elements
+    return (
+      <div>Im a react component</div>
+    )
+  }
+}
+
+// Substantiate a react component and tell it where on the DOM it should attach itself
+ReactDom.render(<App />, document.querySelector('.container'))
