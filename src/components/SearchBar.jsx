97cc52dd42609f2c048b9a0f1c71aa23aee76366
Dennis Bailey <bailey.dennis@gmail.com>
2016-04-23 12:52:17 -0600

    Added an onchange handler to the searchbar

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -14,6 +14,7 @@ class SearchBar extends Component {
       <div className="search-bar">
         <input 
           value={this.state.term}
+          onChange={event => this.onInputChange(event.target.value)}
         />
       </div> 
     )
