e0eae4c378d5500000da323252b49f9930bce987
Dennis Bailey <bailey.dennis@gmail.com>
2016-04-23 13:14:57 -0600

    search with a placeholder value works

--- src/index.js
+++ src/index.js
@@ -9,12 +9,32 @@ import ReactDom from 'react-dom';
 // Realtive path to the component
 import SearchBar from './components/SearchBar';
 
+// import the YouTubes Search API module 
+import YTSearch from 'youtube-api-search';
+
+// import the YouTubes API Key
+import { apiKey } from '../config.js';
+
 
 // Alternate method to extend App
 // Let Component = React.Component
 // class App extends Component
 
 class App extends React.Component {
+  
+  constructor() {
+    super();
+    
+    this.state = { videos: [], 
+                   selectedVideo: null };
+                   
+                   this.videoSearch('Firewatch');
+  }
+  
+  videoSearch(term) {
+    YTSearch({ key: apiKey, term: term}, (videos) => {console.log(videos)});
+  }
+  
   // You must have a render funtion within every react component
   render() {
     // render components can only return one element. But that element can wrap around multiple elements
